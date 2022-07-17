3d989de56e359c7ee55f60771889c662e8ad7a30
edsonalvesdev <edson.f.alves09@gmail.com>
2022-07-17 20:08:07 -0300

    C4E1, C4E2, C5E1, C6E1, C7E1

---
+++ Udemy/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy {
+    internal class Cliente {
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public override string ToString() { 
+            return Room.ToString() + ": " + Name + ", " + Email;
+        }
+    }
+}
