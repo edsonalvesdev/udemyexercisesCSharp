9c9a162c94c2abf9be329a4cf4943452269b410e
edsonalvesdev <edson.f.alves09@gmail.com>
2022-08-14 01:54:44 -0300

    Criado p projeto ExerciseCap9. Criadas as classes OrderStatus, OrderItem, Product, Client e Order.

---
+++ ExerciseCap9/Entities/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCap9.Entities
+{
+    internal class OrderItem
+    {
+        public Product Product { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(Product product, double price, int quantity)
+        {
+            Product = product;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
